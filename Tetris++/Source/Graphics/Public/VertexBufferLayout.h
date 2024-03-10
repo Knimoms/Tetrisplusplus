610e9c9529aebfa95c374e26b476b36fe910ac10
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-10 23:14:25 +0100

    VertexBufferLayout class added

--- Tetris++/Source/Graphics/Public/VertexBufferLayout.h
+++ Tetris++/Source/Graphics/Public/VertexBufferLayout.h
@@ -23,3 +23,45 @@ struct VertexBufferElement
 		return 0;
 	}
 };
+
+class VertexBufferLayout
+{
+private:
+	std::vector<VertexBufferElement> m_VertexBufferElements;
+	unsigned int m_Stride;
+
+public:
+	VertexBufferLayout()
+		: m_Stride(0) {};
+
+	template <typename T>
+	void Push(unsigned int count)
+	{
+		ASSERT(false);
+	}
+
+	template <>
+	void Push<float>(unsigned int count)
+	{
+		m_VertexBufferElements.push_back({ GL_FLOAT, count, GL_FALSE });
+		m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
+	}
+
+	template <>
+	void Push<unsigned int>(unsigned int count)
+	{
+		m_VertexBufferElements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
+		m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * count;
+	}
+
+	template <>
+	void Push<unsigned char>(unsigned int count)
+	{
+		m_VertexBufferElements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
+		m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * count;
+	}
+
+	inline const std::vector<VertexBufferElement>& GetVertexBufferElements() const { return m_VertexBufferElements; }
+	inline unsigned int GetStride() const { return m_Stride; }
+};
+
