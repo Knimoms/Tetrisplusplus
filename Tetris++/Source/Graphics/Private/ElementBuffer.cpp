8944648f3442563779e8aed7cac7b62c0fbdaf09
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-10 22:42:47 +0100

    ElementBuffer class added

---
+++ Tetris++/Source/Graphics/Private/ElementBuffer.cpp
@@ -0,0 +1,25 @@
+#include "ElementBuffer.h"
+#include <GL/glew.h>
+
+ElementBuffer::ElementBuffer(std::vector<unsigned int>& indices)
+	:m_Count(indices.size())
+{
+	glGenBuffers(1, &m_RendererID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+}
+
+ElementBuffer::~ElementBuffer()
+{
+	glDeleteBuffers(1, &m_RendererID);
+}
+
+void ElementBuffer::Bind() const
+{
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+}
+
+void ElementBuffer::Unbind()
+{
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
