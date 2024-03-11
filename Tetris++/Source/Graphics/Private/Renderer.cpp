e1d8cbaa6e59af7ca69a45b3265a7fb7c492be8f
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-11 23:12:39 +0100

    Renderer class added

---
+++ Tetris++/Source/Graphics/Private/Renderer.cpp
@@ -0,0 +1,38 @@
+#include "Renderer.h"
+#include "Mesh.h"
+
+#include "GL/glew.h"
+
+Renderer::Renderer()
+	:m_DefaultShader("Resources/Shader/Default.vert", "Resources/Shader/Default.frag")
+{
+}
+
+void Renderer::DrawMesh(const Mesh& inMesh)
+{
+	inMesh.Bind();
+	m_DefaultShader.Bind();
+	glDrawElements(GL_TRIANGLES, inMesh.GetElementBuffer().GetCount(), GL_UNSIGNED_INT, nullptr);
+	Mesh::Unbind();
+	Shader::Unbind();
+}
+
+void Renderer::DrawMesh(const Mesh& inMesh, const Shader& inShader)
+{
+	inMesh.Bind();
+	inShader.Bind();
+	glDrawElements(GL_TRIANGLES, inMesh.GetElementBuffer().GetCount(), GL_UNSIGNED_INT, nullptr);
+	Mesh::Unbind();
+	Shader::Unbind();
+}
+
+void Renderer::SetBackgroundColor(const glm::vec3& inColor)
+{
+	m_BackgroundColor = inColor;
+}
+
+void Renderer::Clear()
+{
+	glClearColor(m_BackgroundColor.x, m_BackgroundColor.y, m_BackgroundColor.z, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
