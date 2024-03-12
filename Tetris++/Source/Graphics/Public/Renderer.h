3f8ef034c674879ddb3ab6a37014f6776bc250fe
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-12 22:23:45 +0100

    Changed projection matrix
    left is 0, right 10, top 0 and bottom 20

--- Tetris++/Source/Graphics/Public/Renderer.h
+++ Tetris++/Source/Graphics/Public/Renderer.h
@@ -10,7 +10,7 @@ class Renderer
 {
 private:
 	glm::vec3 m_BackgroundColor = { 0.f, 0.f, 0.f };
-	glm::mat4 m_ProjectionMatrix = glm::ortho(-5.f, 5.f, -10.f, 10.f, -1.f, 1.f);
+	glm::mat4 m_ProjectionMatrix = glm::ortho(0.f, 10.f, 20.f, 0.f, -1.f, 1.f);
 
 public:
 	void DrawMesh(const Mesh& inMesh, const Transform& inTransform = { {0.f, 0.f} , 0.f }, Shader& inShader = Shader::GetDefaultShader());
