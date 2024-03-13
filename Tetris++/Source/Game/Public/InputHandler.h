9d409930e412aa0b8096e3617ca43616fcc0d359
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-13 12:07:19 +0100

    InputHandler class added
    checks for pressed keys and calls function for them

--- Tetris++/Source/Game/Public/InputHandler.h
+++ Tetris++/Source/Game/Public/InputHandler.h
@@ -24,3 +24,30 @@ struct InputMemberFunction : InputFunction
 		(object->*method)();
 	}
 };
+
+#include <unordered_map>
+struct GLFWwindow;
+
+class InputHandler
+{
+private:
+	std::unordered_map<int, bool> m_PressedKeys;
+	std::unordered_map<int, InputFunction*> m_KeyFunctions;
+	GLFWwindow* m_InputWindow;
+
+public:
+	InputHandler(GLFWwindow* inWindow);
+
+	template<typename T>
+	void AddInput(int key, T* object, void (T::* MemberFunction)(void))
+	{
+		InputMemberFunction<T>* newInput = new InputMemberFunction(object, MemberFunction);
+		m_KeyFunctions[key] = newInput;
+		m_PressedKeys[key] = false;
+	}
+
+	void RemoveInput(int key);
+
+	void Tick(float DeltaSeconds);
+
+};
