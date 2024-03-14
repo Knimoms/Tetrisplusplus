4e1c3bc8d069d206ec7e6202250407005d681dbd
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-14 17:50:13 +0100

    Added UpdateEvent to Game

--- Tetris++/Source/Game/Public/Game.h
+++ Tetris++/Source/Game/Public/Game.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "Event.h"
+
 #include <string>
+
 struct GLFWwindow;
 
 class Game
@@ -10,11 +13,16 @@ private:
 	int m_WindowWidth;
 	int m_WindowHeight;
 
+	Event<float> m_UpdateEvent;
+
 public:
 	Game(const std::string& gameName, int windowWidth, int windowHeight);
 
 	void Run();
 	GLFWwindow* CreateWindow();
 	bool SetupOpenGLSettings(); //returns if Setup was successful
+
+	inline Event<float>& GetUpdateEvent() { return m_UpdateEvent; }
+
 };
 
