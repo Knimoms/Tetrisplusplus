758b6ca3a831379eec9510e0357f1856cc4788a6
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-14 12:24:58 +0100

    Command template classes added

---
+++ Tetris++/Source/Framework/Public/Command.h
@@ -0,0 +1,52 @@
+#pragma once
+
+template<typename T>
+class Command
+{
+public:
+	virtual void Execute(<T>) = 0;
+};
+
+template<typename T, typename U>
+class ObjectCommand : Command<U>
+{
+private:
+	typedef void (T::* MemberFunction)(U);
+
+	T* m_Object;
+	MemberFunction m_Method;
+
+public:
+
+	ObjectCommand(T* inObject, MemberFunction inMethod)
+		:m_Object(inObject), m_Method(inMethod)
+	{
+	}
+
+	virtual void Execute(U param1) override
+	{
+		(m_Object->*m_Method)(param1);
+	}
+};
+
+template<typename T>
+class ObjectCommand<T, void> : Command<void>
+{
+private:
+	typedef void (T::* MemberFunction)(U);
+
+	T* m_Object;
+	MemberFunction m_Method;
+
+public:
+
+	ObjectCommand(T* inObject, MemberFunction inMethod)
+		:m_Object(inObject), m_Method(inMethod)
+	{
+	}
+
+	virtual void Execute() override
+	{
+		(m_Object->*m_Method)();
+	}
+};
