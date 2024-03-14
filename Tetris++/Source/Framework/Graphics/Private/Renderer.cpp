b0564e55c4c911a217f3b0658128be08b95a9e38
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-14 23:15:40 +0100

    Renderer draws all RenderEntries on RenderFrame call

--- Tetris++/Source/Framework/Graphics/Private/Renderer.cpp
+++ Tetris++/Source/Framework/Graphics/Private/Renderer.cpp
@@ -1,19 +1,44 @@
 #include "Renderer.h"
+#include "Mesh.h"
+#include "Shader.h"
 
 #include "GL/glew.h"
 
-void Renderer::DrawMesh(const Mesh& inMesh, const Transform& inTransform, Shader& inShader)
+void Renderer::RenderFrame()
 {
-	inMesh.Bind();
-	inShader.Bind();
+	size_t renderEntriesNum = m_RenderEntries.size();
 
-	glm::mat4 model = glm::translate(glm::mat4(1), { inTransform.position, 0.f });
-	model = glm::rotate(model, glm::radians(inTransform.rotation), { 0, 0, 1 });
+	for (int i = 0; i < renderEntriesNum; ++i)
+		DrawRenderEntry(m_RenderEntries[i]);
+}
+
+void Renderer::DrawRenderEntry(RenderEntry& renderEntry)
+{
+	renderEntry.mesh->Bind();
+	renderEntry.shader->Bind();
+
+	glm::mat4 model = glm::translate(glm::mat4(1), { renderEntry.transform->position, 0.f });
+	model = glm::rotate(model, glm::radians(renderEntry.transform->rotation), { 0, 0, 1 });
 	glm::mat4 mvp = m_ProjectionMatrix * model;
-	inShader.SetUniformMat4f("u_MVP", mvp);
-	glDrawElements(GL_TRIANGLES, inMesh.GetElementBuffer().GetCount(), GL_UNSIGNED_INT, nullptr);
+	
+	renderEntry.shader->SetUniformMat4f("u_MVP", mvp);
+	glDrawElements(GL_TRIANGLES, renderEntry.mesh->GetElementBuffer().GetCount(), GL_UNSIGNED_INT, nullptr);
+
 	Mesh::Unbind();
 	Shader::Unbind();
+}
+
+void Renderer::AddRenderEntry(Mesh* inMesh, Transform* inTransform, Shader* inShader)
+{
+	m_RenderEntries.push_back({ inMesh, inTransform, inShader });
+}
+
+void Renderer::RemoveRenderEntry(Mesh* inMesh)
+{
+	auto position = std::find_if(m_RenderEntries.begin(), m_RenderEntries.end(),
+		[inMesh](const RenderEntry& renderEntry) -> bool { return renderEntry.mesh == inMesh; });
+
+	m_RenderEntries.erase(position);
 }
 
 void Renderer::SetBackgroundColor(const glm::vec3& inColor)
--- Tetris++/Source/Framework/Graphics/Public/Renderer.h
+++ Tetris++/Source/Framework/Graphics/Public/Renderer.h
@@ -1,19 +1,32 @@
 #pragma once
-#include "Shader.h"
-#include "Mesh.h"
-
-#include "vector"
-#include "glm/glm.hpp"
+#include <vector>
 #include "glm/gtc/matrix_transform.hpp"
+
+class Mesh;
+struct Transform;
+class Shader;
+
+struct RenderEntry
+{
+	Mesh* mesh;
+	Transform* transform;
+	Shader* shader;
+};
 
 class Renderer
 {
 private:
 	glm::vec3 m_BackgroundColor = { 0.f, 0.f, 0.f };
 	glm::mat4 m_ProjectionMatrix = glm::ortho(0.f, 10.f, 20.f, 0.f, -1.f, 1.f);
+	std::vector<RenderEntry> m_RenderEntries;
 
 public:
-	void DrawMesh(const Mesh& inMesh, const Transform& inTransform = { {0.f, 0.f} , 0.f }, Shader& inShader = Shader::GetDefaultShader());
+	void RenderFrame();
+	void DrawRenderEntry(RenderEntry& renderEntry);
+
+	void AddRenderEntry(Mesh* inMesh, Transform* inTransform, Shader* inShader);
+	void RemoveRenderEntry(Mesh* inMesh);
+
 	void SetBackgroundColor(const glm::vec3& inColor);
 	void Clear();
 
