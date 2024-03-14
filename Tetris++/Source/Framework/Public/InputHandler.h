714fb2ed391060c160146f136617f12f748ae67f
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-14 20:16:47 +0100

    Modified InputHandler class to use KeyCommand

--- Tetris++/Source/Framework/Public/InputHandler.h
+++ Tetris++/Source/Framework/Public/InputHandler.h
@@ -6,17 +6,19 @@
 
 enum KeyAction
 {
-	PRESSED,
-	RELEASE
+	RELEASED,
+	PRESSED
 };
 
 struct KeyCommand
 {
-	std::shared_ptr<Command<void>> Command;
+	int key;
 	KeyAction executeAction;
+	std::shared_ptr<Command<void>> Command;
 };
 
 #include <unordered_map>
+#include <string>
 
 struct GLFWwindow;
 
@@ -24,21 +26,20 @@ class InputHandler
 {
 private:
 	std::unordered_map<int, bool> m_PressedKeys;
-	std::unordered_map<int, KeyCommand> m_KeyFunctions;
+	std::vector<KeyCommand> m_KeyCommands;
 	GLFWwindow* m_InputWindow;
 
 public:
 	InputHandler(GLFWwindow* inWindow);
 
 	template<typename T>
-	void AddInput(int key, T* object, void (T::* memberFunction)(void), KeyAction executeOn)
+	void AddInput(int key, KeyAction executeOn, T* inObject, void (T::* inMethod)(void))
 	{
-		Command<T>* newInput = new Command<>(object, memberFunction);
-		m_KeyFunctions[key] = { std::shared_ptr<Command<void>>, executeOn };
+		m_KeyCommands.push_back({ key, executeOn, std::shared_ptr<ObjectCommand<T, void>>(new ObjectCommand<T, void>(inObject, inMethod))});
 		m_PressedKeys[key] = false;
 	}
 
-	void RemoveInput(int key);
+	void RemoveInput(int key, KeyAction executeOn);
 
 	void KeyboardInputTick();
 
