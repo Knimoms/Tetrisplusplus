c113d125a70cde9b6242e17bdbb0068bf72f271d
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-14 18:16:04 +0100

    InputHandler added KeyAction enum and KeyCommand struct
    Replaced the InputFunction with the Command class and a KeyAction enum

--- Tetris++/Source/Framework/Public/InputHandler.h
+++ Tetris++/Source/Framework/Public/InputHandler.h
@@ -1,53 +1,45 @@
 #pragma once
 
-class InputFunction
+#include "Command.h"
+
+#include <memory>
+
+enum KeyAction
 {
-public:
-	virtual void CallFunction() = 0;
+	PRESSED,
+	RELEASE
 };
 
-template<typename T>
-struct InputMemberFunction : InputFunction
+struct KeyCommand
 {
-	typedef void (T::* MemberFunction)(void);
-
-	T* object;
-	MemberFunction method;
-
-	InputMemberFunction(T* inObject, MemberFunction inMethod)
-		:object(inObject), method(inMethod)
-	{
-	}
-
-	virtual void CallFunction() override
-	{
-		(object->*method)();
-	}
+	std::unique_ptr<Command<void>> Command;
+	KeyAction executeAction;
 };
 
 #include <unordered_map>
+
 struct GLFWwindow;
 
 class InputHandler
 {
 private:
 	std::unordered_map<int, bool> m_PressedKeys;
-	std::unordered_map<int, InputFunction*> m_KeyFunctions;
+	std::unordered_map<int, KeyCommand> m_KeyFunctions;
 	GLFWwindow* m_InputWindow;
 
 public:
 	InputHandler(GLFWwindow* inWindow);
 
 	template<typename T>
-	void AddInput(int key, T* object, void (T::* MemberFunction)(void))
+	void AddInput(int key, T* object, void (T::* memberFunction)(void), KeyAction executeOn)
 	{
-		InputMemberFunction<T>* newInput = new InputMemberFunction(object, MemberFunction);
-		m_KeyFunctions[key] = newInput;
+		Command<T>* newInput = new Command<>(object, memberFunction);
+		m_KeyFunctions[key] = { std::unique_ptr<Command<void>>, executeOn };
 		m_PressedKeys[key] = false;
 	}
 
 	void RemoveInput(int key);
 
-	void Tick(float DeltaSeconds);
+	void KeyboardInputTick();
 
 };
