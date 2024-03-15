9e7fff2b5db3fd45b1f31c8aaca30fdf21c6b181
Timon Albrecht <timon-albrecht@outlook.de>
2024-03-15 15:07:22 +0100

    setting up m_AllTetrominoShapes in GameMode

--- Tetris++/Source/Game/Private/GameMode.cpp
+++ Tetris++/Source/Game/Private/GameMode.cpp
@@ -6,7 +6,72 @@
 GameMode::GameMode()
 	:m_RNG(std::bind(std::uniform_int_distribution<int>(0, 6), std::mt19937(std::chrono::high_resolution_clock::now().time_since_epoch().count())))
 {
-	
+	m_AllTetrominoShapes =
+	{
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 0, 1, 1, 1, 1,
+			 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0},
+			{0.f, 240.f / 255.f, 0.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 1, 0, 0, 0,
+			 0, 1, 1, 1, 0,
+			 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0},
+			{0.f, 0.f, 240.f / 255.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 1, 0,
+			 0, 1, 1, 1, 0,
+			 0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0},
+			{240.f / 255.f, 160.f / 255.f, 0.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 0, 0, 1, 1, 0,
+			 0, 0, 1, 1, 0,
+			 0, 0, 0, 0, 0},
+			{240.f / 255.f, 240.f / 255.f, 0.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 0, 0, 1, 1, 0,
+			 0, 1, 1, 0, 0,
+			 0, 0, 0, 0, 0},
+			{0.f, 240.f / 255.f, 0.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 0, 0, 1, 1, 0,
+			 0, 1, 1, 0, 0,
+			 0, 0, 0, 0, 0},
+			{160.f / 255.f, 0.f, 240.f / 255.f}
+		}),
+		ShapeColorCombination(
+		{
+			{0, 0, 0, 0, 0,
+			 0, 0, 0, 0, 0,
+			 0, 1, 1, 0, 0,
+			 0, 0, 1, 1, 0,
+			 0, 0, 0, 0, 0},
+			{240.f / 255.f, 0.f, 0.f}
+		})
+	};
 
 }
 
@@ -17,5 +82,5 @@ void GameMode::Update(float DeltaTimeSeconds)
 
 std::shared_ptr<Tetromino> GameMode::SpawnRandomTetromino()
 {
-
+	
 }
